b3df815b8d07ac7f468ea58b842d7c485bc5221c
sugamadhiakri <sugam.adhiakri123@gmail.com>
2021-08-20 20:30:34 +0545

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+import { MovieResolver } from "./resolvers/MovieResolver";
+
+(async () => {
+	const app = express();
+
+	await createConnection();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloWorldResolver, MovieResolver],
+		}),
+		context: ({ req, res }) => ({ req, res }),
+	});
+
+	await apolloServer.start();
+	apolloServer.applyMiddleware({ app, cors: false });
+
+	app.listen(4000, () => {
+		console.log("express server started");
+	});
+})();
